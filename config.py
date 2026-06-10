858c22814298e18cc7d4afcc550dd6ac2a27623b
Jess Desmond <Jdes_4@hotmail.co.uk>
2026-06-10 16:30:16 +0100

    temporarily removed prt 22 from suspicious ports due to ssh connection for config

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ ADMIN_PASSWORD = os.getenv("IDS_ADMIN_PASSWORD", "password")
 SECRET_KEY = os.getenv("IDS_SECRET_KEY", "dev-secret-key")
 INTERFACE = os.getenv("IDS_INTERFACE", "")
 
-SUSPICIOUS_PORTS = [21, 22, 23, 445, 3389, 5900, 8080]
+SUSPICIOUS_PORTS = [21, 23, 445, 3389, 5900, 8080]
 BLACKLISTED_IPS = [ip.strip() for ip in os.getenv("IDS_BLACKLISTED_IPS", "").split(",") if ip.strip()]
