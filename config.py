651487a8c9e8995e90a5bd9ba798baaa698fbf47
Jess Desmond <Jdes_4@hotmail.co.uk>
2026-06-10 17:37:02 +0100

    config oopsie

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DB_PATH = Path(os.getenv("IDS_DB_PATH", BASE_DIR / "ids_data.sqlite"))
 ADMIN_USER = os.getenv("IDS_ADMIN_USER", "admin")
 ADMIN_PASSWORD = os.getenv("IDS_ADMIN_PASSWORD", "password")
 SECRET_KEY = os.getenv("IDS_SECRET_KEY", "dev-secret-key")
-INTERFACE = os.getenv("IDS_INTERFACE", "wlan0,etho0")
+INTERFACE = os.getenv("IDS_INTERFACE", "wlan0,eth0")
 
 SUSPICIOUS_PORTS = [21, 23, 445, 3389, 5900, 8080]
 BLACKLISTED_IPS = [ip.strip() for ip in os.getenv("IDS_BLACKLISTED_IPS", "").split(",") if ip.strip()]
