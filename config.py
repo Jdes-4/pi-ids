4cc079b4e1d82b94ffb207dc05fdf55dc2a8f936
Jess Desmond <Jdes_4@hotmail.co.uk>
2026-06-17 18:53:54 +0100

    changed admin pass to admin pass hash

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 DB_PATH = Path(os.getenv("IDS_DB_PATH", BASE_DIR / "ids_data.sqlite"))
 ADMIN_USER = os.getenv("IDS_ADMIN_USER")
-ADMIN_PASSWORD = os.getenv("IDS_ADMIN_PASSWORD_HASH")
+ADMIN_PASSWORD_HASH = os.getenv("IDS_ADMIN_PASSWORD_HASH")
 SECRET_KEY = os.getenv("IDS_SECRET_KEY")
 INTERFACE = os.getenv("IDS_INTERFACE", "wlan0,eth0")
 
